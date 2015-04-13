d523e492da9c83b988d992f2ba07c552b906044b
Yoichi Imai <sunnyone41@gmail.com>
2015-04-14 00:07:46 +0900

    Introduce Caliburn.Micro.

--- ScreenCaptureWrapper/ShellView.xaml.cs
+++ ScreenCaptureWrapper/ShellView.xaml.cs
@@ -10,17 +10,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ScreenCaptureWrapper
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class ShellView : Window
     {
-        public MainWindow()
+        public ShellView()
         {
             InitializeComponent();
         }
---
+++ ScreenCaptureWrapper/ShellViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCaptureWrapper
+{
+    public class ShellViewModel : Caliburn.Micro.PropertyChangedBase
+    {
+        private int videoX;
+        public int VideoX
+        {
+            get { return videoX; }
+            set { videoX = value; NotifyOfPropertyChange(() => VideoX); }
+        }
+
+        private int videoY;
+        public int VideoY
+        {
+            get { return videoY; }
+            set { videoY = value; NotifyOfPropertyChange(() => VideoY);  }
+        }
+
+        private int videoWidth;
+        public int VideoWidth
+        {
+            get { return videoWidth; }
+            set { videoWidth = value; NotifyOfPropertyChange(() => VideoWidth); }
+        }
+
+        private int videoHeight;
+        public int VideoHeight
+        {
+            get { return videoHeight; }
+            set { videoHeight = value; NotifyOfPropertyChange(() => VideoHeight); }
+        }
+        
+        public void SelectFile()
+        {
+        }
+
+        public void PickPosition()
+        {
+            this.VideoX = 100;
+            this.VideoY = 100;
+            this.VideoHeight = 100;
+            this.VideoWidth = 100;
+        }
+
+        public bool CanRecord { get { return true; } }
+        public void Record()
+        {
+
+        }
+
+        public bool CanStop { get { return false; } }
+        public void Stop()
+        {
+
+        }
+    }
+}
