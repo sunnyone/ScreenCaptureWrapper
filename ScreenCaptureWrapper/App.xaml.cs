97275e74c74839844e543ac0db37723075c2d6b1
Yoichi Imai <sunnyone41@gmail.com>
2015-04-14 23:01:35 +0900

    Removed WPFThemes.

--- ScreenCaptureWrapper/App.xaml.cs
+++ ScreenCaptureWrapper/App.xaml.cs
@@ -8,14 +8,7 @@ using System.Windows;
 
 namespace ScreenCaptureWrapper
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            WPF.Themes.ThemeManager.ApplyTheme(this, "BureauBlue");
-        }
     }
 }
